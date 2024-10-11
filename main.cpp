9b1c80d186025401a47126b3d60e4bbde7006364
Sergey Litvinov <slitvinov@gmail.com>
2024-10-11 11:23:58 +0200

    up

--- main.cpp
+++ main.cpp
@@ -20,30 +20,10 @@
 #include <cstdlib>
 #include <mpi.h>
 
-#if defined(LAMMPS_TRAP_FPE) && defined(_GNU_SOURCE)
-#include <fenv.h>
-#endif
-
-#include <execinfo.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 using namespace LAMMPS_NS;
-
-void handler(int sig)
-{
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
 
 // for convenience
 static void finalize()
@@ -56,20 +36,9 @@ static void finalize()
 
 int main(int argc, char **argv)
 {
-  signal(SIGSEGV, handler);    // install our handler
   MPI_Init(&argc, &argv);
   MPI_Comm lammps_comm = MPI_COMM_WORLD;
 
-#if defined(LAMMPS_TRAP_FPE) && defined(_GNU_SOURCE)
-  // enable trapping selected floating point exceptions.
-  // this uses GNU extensions and is only tested on Linux
-  // therefore we make it depend on -D_GNU_SOURCE, too.
-  fesetenv(FE_NOMASK_ENV);
-  fedisableexcept(FE_ALL_EXCEPT);
-  feenableexcept(FE_DIVBYZERO);
-  feenableexcept(FE_INVALID);
-  feenableexcept(FE_OVERFLOW);
-#endif
 
   try {
     auto lammps = new LAMMPS(argc, argv, lammps_comm);
