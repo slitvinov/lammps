dbec7433ebba60f724ce5e2ea68b11c0a50adbd7
Sergey Litvinov <slitvinov@gmail.com>
2024-10-11 16:56:07 +0200

    up

--- variable.h
+++ variable.h
@@ -9,7 +9,6 @@ class Variable : protected Pointers {
   Variable(class LAMMPS *);
   ~Variable() override;
   void set(int, char **);
-  int set_string(const char *, const char *);
   int next(int, char **);
   int find(const char *);
   int equalstyle(int);
