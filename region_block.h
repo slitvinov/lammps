bc3790323fb8e2a3ed8da46a0f37114e10559bf7
Sergey Litvinov <slitvinov@gmail.com>
2024-10-13 13:26:44 +0200

    variable.h

--- region_block.h
+++ region_block.h
@@ -12,9 +12,6 @@ class RegBlock : public Region {
   ~RegBlock() override;
   void init() override;
   int inside(double, double, double) override;
-  int surface_interior(double *, double) override;
-  int surface_exterior(double *, double) override;
-  void shape_update() override;
  protected:
   double xlo, xhi, ylo, yhi, zlo, zhi;
   double corners[6][4][3];
@@ -24,9 +21,6 @@ class RegBlock : public Region {
   int zlostyle, zlovar, zhistyle, zhivar;
   char *xlostr, *ylostr, *zlostr;
   char *xhistr, *yhistr, *zhistr;
-  double find_closest_point(int, double *, double &, double &, double &);
-  int inside_face(double *, int);
-  void variable_check();
 };
 }
 #endif
