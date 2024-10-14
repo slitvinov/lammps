d3eace9d1eccb48e97774821ffe1fd532f334ca7
Sergey Litvinov <slitvinov@gmail.com>
2024-10-14 22:58:51 +0200

    rm hashlittle

--- hashlittle.h
+++
@@ -1,8 +0,0 @@
-#ifndef LMP_HASHLITTLE_H
-#define LMP_HASHLITTLE_H
-#include <cstddef>
-#include <cstdint>
-namespace LAMMPS_NS {
-uint32_t hashlittle(const void *key, size_t length, uint32_t);
-}
-#endif
