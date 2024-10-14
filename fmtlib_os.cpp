91249e4a48c23ea51167a87c3c7a2eb642b428f8
Sergey Litvinov <slitvinov@gmail.com>
2024-10-14 14:54:07 +0200

    rm _WIN32

--- fmtlib_os.cpp
+++ fmtlib_os.cpp
@@ -6,150 +6,15 @@
 #if FMT_USE_FCNTL
 #include <sys/stat.h>
 #include <sys/types.h>
-#ifndef _WIN32
 #include <unistd.h>
-#else
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <io.h>
-#ifndef S_IRUSR
-#define S_IRUSR _S_IREAD
-#endif
-#ifndef S_IWUSR
-#define S_IWUSR _S_IWRITE
-#endif
-#ifndef S_IRGRP
-#define S_IRGRP 0
-#endif
-#ifndef S_IWGRP
-#define S_IWGRP 0
-#endif
-#ifndef S_IROTH
-#define S_IROTH 0
-#endif
-#ifndef S_IWOTH
-#define S_IWOTH 0
-#endif
-#endif
-#endif
-#ifdef _WIN32
-#include <windows.h>
 #endif
 namespace {
-#ifdef _WIN32
-using rwresult = int;
-inline unsigned convert_rwcount(std::size_t count) {
-  return count <= UINT_MAX ? static_cast<unsigned>(count) : UINT_MAX;
-}
-#elif FMT_USE_FCNTL
+#if   FMT_USE_FCNTL
 using rwresult = ssize_t;
 inline std::size_t convert_rwcount(std::size_t count) { return count; }
 #endif
 } // namespace
 FMT_BEGIN_NAMESPACE
-#ifdef _WIN32
-detail::utf16_to_utf8::utf16_to_utf8(basic_string_view<wchar_t> s) {
-  if (int error_code = convert(s)) {
-    FMT_THROW(windows_error(error_code,
-                            "cannot convert string from UTF-16 to UTF-8"));
-  }
-}
-int detail::utf16_to_utf8::convert(basic_string_view<wchar_t> s) {
-  if (s.size() > INT_MAX)
-    return ERROR_INVALID_PARAMETER;
-  int s_size = static_cast<int>(s.size());
-  if (s_size == 0) {
-    buffer_.resize(1);
-    buffer_[0] = 0;
-    return 0;
-  }
-  int length = WideCharToMultiByte(CP_UTF8, 0, s.data(), s_size, nullptr, 0,
-                                   nullptr, nullptr);
-  if (length == 0)
-    return GetLastError();
-  buffer_.resize(length + 1);
-  length = WideCharToMultiByte(CP_UTF8, 0, s.data(), s_size, &buffer_[0],
-                               length, nullptr, nullptr);
-  if (length == 0)
-    return GetLastError();
-  buffer_[length] = 0;
-  return 0;
-}
-namespace detail {
-class system_message {
-  system_message(const system_message &) = delete;
-  void operator=(const system_message &) = delete;
-  unsigned long result_;
-  wchar_t *message_;
-  static bool is_whitespace(wchar_t c) noexcept {
-    return c == L' ' || c == L'\n' || c == L'\r' || c == L'\t' || c == L'\0';
-  }
-
-public:
-  explicit system_message(unsigned long error_code)
-      : result_(0), message_(nullptr) {
-    result_ = FormatMessageW(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-        nullptr, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        reinterpret_cast<wchar_t *>(&message_), 0, nullptr);
-    if (result_ != 0) {
-      while (result_ != 0 && is_whitespace(message_[result_ - 1])) {
-        --result_;
-      }
-    }
-  }
-  ~system_message() { LocalFree(message_); }
-  explicit operator bool() const noexcept { return result_ != 0; }
-  operator basic_string_view<wchar_t>() const noexcept {
-    return basic_string_view<wchar_t>(message_, result_);
-  }
-};
-class utf8_system_category final : public std::error_category {
-public:
-  const char *name() const noexcept override { return "system"; }
-  std::string message(int error_code) const override {
-    system_message msg(error_code);
-    if (msg) {
-      utf16_to_utf8 utf8_message;
-      if (utf8_message.convert(msg) == ERROR_SUCCESS) {
-        return utf8_message.str();
-      }
-    }
-    return "unknown error";
-  }
-};
-} // namespace detail
-FMT_API const std::error_category &system_category() noexcept {
-  static const detail::utf8_system_category category;
-  return category;
-}
-std::system_error vwindows_error(int err_code, string_view format_str,
-                                 format_args args) {
-  auto ec = std::error_code(err_code, system_category());
-  return std::system_error(ec, vformat(format_str, args));
-}
-void detail::format_windows_error(detail::buffer<char> &out, int error_code,
-                                  const char *message) noexcept {
-  FMT_TRY {
-    system_message msg(error_code);
-    if (msg) {
-      utf16_to_utf8 utf8_message;
-      if (utf8_message.convert(msg) == ERROR_SUCCESS) {
-        fmt::format_to(buffer_appender<char>(out), "{}: {}", message,
-                       utf8_message);
-        return;
-      }
-    }
-  }
-  FMT_CATCH(...) {}
-  format_error_code(out, error_code, message);
-}
-void report_windows_error(int error_code, const char *message) noexcept {
-  report_error(detail::format_windows_error, error_code, message);
-}
-#endif
 buffered_file::~buffered_file() noexcept {
   if (file_ && FMT_SYSTEM(fclose(file_)) != 0)
     report_system_error(errno, "cannot close file");
@@ -176,17 +41,9 @@ int buffered_file::descriptor() const {
 }
 #if FMT_USE_FCNTL
 file::file(cstring_view path, int oflag) {
-#ifdef _WIN32
-  using mode_t = int;
-#endif
   constexpr mode_t mode =
       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-#if defined(_WIN32) && !defined(__MINGW32__)
-  fd_ = -1;
-  FMT_POSIX_CALL(sopen_s(&fd_, path.c_str(), oflag, _SH_DENYNO, mode));
-#else
   FMT_RETRY(fd_, FMT_POSIX_CALL(open(path.c_str(), oflag, mode)));
-#endif
   if (fd_ == -1)
     FMT_THROW(system_error(errno, "cannot open file {}", path.c_str()));
 }
@@ -203,18 +60,6 @@ void file::close() {
     FMT_THROW(system_error(errno, "cannot close file"));
 }
 long long file::size() const {
-#ifdef _WIN32
-  DWORD size_upper = 0;
-  HANDLE handle = reinterpret_cast<HANDLE>(_get_osfhandle(fd_));
-  DWORD size_lower = FMT_SYSTEM(GetFileSize(handle, &size_upper));
-  if (size_lower == INVALID_FILE_SIZE) {
-    DWORD error = GetLastError();
-    if (error != NO_ERROR)
-      FMT_THROW(windows_error(GetLastError(), "cannot get file size"));
-  }
-  unsigned long long long_size = size_upper;
-  return (long_size << sizeof(DWORD) * CHAR_BIT) | size_lower;
-#else
   using Stat = struct stat;
   Stat file_stat = Stat();
   if (FMT_POSIX_CALL(fstat(fd_, &file_stat)) == -1)
@@ -222,7 +67,6 @@ long long file::size() const {
   static_assert(sizeof(long long) >= sizeof(file_stat.st_size),
                 "return type of file::size is not large enough");
   return file_stat.st_size;
-#endif
 }
 std::size_t file::read(void *buffer, std::size_t count) {
   rwresult result = 0;
@@ -262,12 +106,7 @@ void file::pipe(file &read_end, file &write_end) {
   read_end.close();
   write_end.close();
   int fds[2] = {};
-#ifdef _WIN32
-  enum { DEFAULT_CAPACITY = 65536 };
-  int result = FMT_POSIX_CALL(pipe(fds, DEFAULT_CAPACITY, _O_BINARY));
-#else
   int result = FMT_POSIX_CALL(pipe(fds));
-#endif
   if (result != 0)
     FMT_THROW(system_error(errno, "cannot create pipe"));
   read_end = file(fds[0]);
@@ -287,16 +126,10 @@ buffered_file file::fdopen(const char *mode) {
   return bf;
 }
 long getpagesize() {
-#ifdef _WIN32
-  SYSTEM_INFO si;
-  GetSystemInfo(&si);
-  return si.dwPageSize;
-#else
   long size = FMT_POSIX_CALL(sysconf(_SC_PAGESIZE));
   if (size < 0)
     FMT_THROW(system_error(errno, "cannot get memory page size"));
   return size;
-#endif
 }
 FMT_API void ostream::grow(size_t) {
   if (this->size() == this->capacity())
