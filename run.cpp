9a25adf16bcd51dee99f476b33dc60124be85079
Sergey Litvinov <slitvinov@gmail.com>
2024-10-14 10:51:09 +0200

    up run.cpp

--- run.cpp
+++ run.cpp
@@ -9,14 +9,7 @@
 using namespace LAMMPS_NS;
 Run::Run(LAMMPS *lmp) : Command(lmp) {}
 void Run::command(int narg, char **arg) {
-  if (narg < 1)
-    utils::missing_cmd_args(FLERR, "run", error);
-  if (domain->box_exist == 0)
-    error->all(FLERR, "Run command before simulation box is defined");
   bigint nsteps_input = utils::bnumeric(FLERR, arg[0], false, lmp);
-  int uptoflag = 0;
-  int startflag = 0;
-  int stopflag = 0;
   bigint start, stop;
   int preflag = 1;
   int postflag = 1;
@@ -25,34 +18,19 @@ void Run::command(int narg, char **arg) {
   int first, last;
   int iarg = 1;
   int nsteps;
-  if (!uptoflag) {
-    if (nsteps_input < 0 || nsteps_input > MAXSMALLINT)
-      error->all(FLERR, "Invalid run command N value: {}", nsteps_input);
-    nsteps = static_cast<int>(nsteps_input);
-  }
-  char **commands = nullptr;
+  nsteps = static_cast<int>(nsteps_input);
   update->whichflag = 1;
-  if (nevery == 0) {
-    update->nsteps = nsteps;
-    update->firststep = update->ntimestep;
-    update->laststep = update->ntimestep + nsteps;
-    if (update->laststep < 0 || update->laststep < update->firststep)
-      error->all(FLERR, "Too many timesteps");
-    if (startflag)
-      update->beginstep = start;
-    else
-      update->beginstep = update->firststep;
-    if (stopflag)
-      update->endstep = stop;
-    else
-      update->endstep = update->laststep;
-    if (preflag || update->first_update == 0) {
-      lmp->init();
-      update->integrate->setup(1);
-    }
-    update->integrate->run(nsteps);
-    update->integrate->cleanup();
+  update->nsteps = nsteps;
+  update->firststep = update->ntimestep;
+  update->laststep = update->ntimestep + nsteps;
+  update->beginstep = update->firststep;
+  update->endstep = update->laststep;
+  if (preflag || update->first_update == 0) {
+    lmp->init();
+    update->integrate->setup(1);
   }
+  update->integrate->run(nsteps);
+  update->integrate->cleanup();
   update->whichflag = 0;
   update->firststep = update->laststep = 0;
   update->beginstep = update->endstep = 0;
