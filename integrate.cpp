e8ecfe710ea9241ec402a4e5d44cb8669abfe99b
Sergey Litvinov <slitvinov@gmail.com>
2024-10-14 14:15:00 +0200

    up

--- integrate.cpp
+++ integrate.cpp
@@ -34,16 +34,6 @@ void Integrate::ev_setup() {
   vlist_global = vlist_atom = cvlist_atom = nullptr;
   nelist_global = nelist_atom = 0;
   nvlist_global = nvlist_atom = ncvlist_atom = 0;
-  if (nelist_global)
-    elist_global = new Compute *[nelist_global];
-  if (nelist_atom)
-    elist_atom = new Compute *[nelist_atom];
-  if (nvlist_global)
-    vlist_global = new Compute *[nvlist_global];
-  if (nvlist_atom)
-    vlist_atom = new Compute *[nvlist_atom];
-  if (ncvlist_atom)
-    cvlist_atom = new Compute *[ncvlist_atom];
   nelist_global = nelist_atom = 0;
   nvlist_global = nvlist_atom = ncvlist_atom = 0;
 }
@@ -52,16 +42,10 @@ void Integrate::ev_set(bigint ntimestep) {
   int tdflag = 0;
   flag = 0;
   int eflag_global = 0;
-  for (i = 0; i < nelist_global; i++)
-    if (elist_global[i]->matchstep(ntimestep))
-      flag = 1;
   if (flag)
     eflag_global = ENERGY_GLOBAL;
   flag = 0;
   int eflag_atom = 0;
-  for (i = 0; i < nelist_atom; i++)
-    if (elist_atom[i]->matchstep(ntimestep))
-      flag = 1;
   if (flag || (tdflag && nelist_atom))
     eflag_atom = ENERGY_ATOM;
   if (eflag_global)
@@ -71,28 +55,11 @@ void Integrate::ev_set(bigint ntimestep) {
   eflag = eflag_global + eflag_atom;
   flag = 0;
   int vflag_global = 0;
-  for (i = 0; i < nvlist_global; i++)
-    if (vlist_global[i]->matchstep(ntimestep))
-      flag = 1;
   if (flag)
     vflag_global = virial_style;
   flag = 0;
   int vflag_atom = 0;
-  for (i = 0; i < nvlist_atom; i++)
-    if (vlist_atom[i]->matchstep(ntimestep))
-      flag = 1;
-  if (flag || (tdflag && nvlist_atom))
-    vflag_atom = VIRIAL_ATOM;
   flag = 0;
   int cvflag_atom = 0;
-  for (i = 0; i < ncvlist_atom; i++)
-    if (cvlist_atom[i]->matchstep(ntimestep))
-      flag = 1;
-  if (flag || (tdflag && ncvlist_atom))
-    cvflag_atom = VIRIAL_CENTROID;
-  if (vflag_global)
-    update->vflag_global = ntimestep;
-  if (vflag_atom || cvflag_atom)
-    update->vflag_atom = ntimestep;
   vflag = vflag_global + vflag_atom + cvflag_atom;
 }
