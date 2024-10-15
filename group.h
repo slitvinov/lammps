c805d35188b652e3d57fea9ab7bbe7a897b0b778
Sergey Litvinov <slitvinov@gmail.com>
2024-10-15 11:52:06 +0200

    up

--- group.h
+++ group.h
@@ -14,38 +14,9 @@ public:
   Group(class LAMMPS *);
   ~Group() override;
   int find(const std::string &);
-  bigint count_all();
-  bigint count(int);
-  bigint count(int, Region *);
-  double mass(int);
-  double mass(int, Region *);
-  double charge(int);
-  double charge(int, Region *);
-  void bounds(int, double *);
-  void bounds(int, double *, Region *);
-  void xcm(int, double, double *);
-  void xcm(int, double, double *, Region *);
-  void vcm(int, double, double *);
-  void vcm(int, double, double *, Region *);
-  void fcm(int, double *);
-  void fcm(int, double *, Region *);
-  double ke(int);
-  double ke(int, Region *);
-  double gyration(int, double, double *);
-  double gyration(int, double, double *, Region *);
-  void angmom(int, double *, double *);
-  void angmom(int, double *, double *, Region *);
-  void torque(int, double *, double *);
-  void torque(int, double *, double *, Region *);
-  void inertia(int, double *, double[3][3]);
-  void inertia(int, double *, double[3][3], Region *);
-  void omega(double *, double[3][3], double *);
-
 private:
   int me;
   std::map<tagint, int> *hash;
-  int find_unused();
-  static void molring(int, char *, void *);
   int molbit;
 };
 } // namespace LAMMPS_NS
