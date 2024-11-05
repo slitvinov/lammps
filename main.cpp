0f0322868a155d059a39baf8cce218ca3c19bcc3
Sergey Litvinov <slitvinov@gmail.com>
2024-11-05 14:30:49 +0100

    up

--- main.cpp
+++ main.cpp
@@ -214,15 +214,9 @@ void LAMMPS::init() {
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm lammps_comm = MPI_COMM_WORLD;
-  try {
-    auto lammps = new LAMMPS(argc, argv, lammps_comm);
-    lammps->input->file();
-    delete lammps;
-  } catch (fmt::format_error &fe) {
-    fprintf(stderr, "fmt::format_error: %s\n", fe.what());
-    MPI_Abort(MPI_COMM_WORLD, 1);
-    exit(1);
-  }
+  auto lammps = new LAMMPS(argc, argv, lammps_comm);
+  lammps->input->file();
+  delete lammps;
   MPI_Barrier(lammps_comm);
   MPI_Finalize();
 }
