533769a99950f914f7365f2f887f5dd961bebf2d
Sergey Litvinov <slitvinov@gmail.com>
2025-05-07 16:05:20 +0200

    up

--- create_box.cpp
+++ create_box.cpp
@@ -27,15 +27,13 @@ void CreateBox::command(int narg, char **arg) {
   domain->box_exist = 1;
   auto region = domain->get_region_by_id(arg[1]);
   region->init();
-  if (strcmp(region->style, "prism") != 0) {
-    domain->triclinic = 0;
-    domain->boxlo[0] = region->extent_xlo;
-    domain->boxhi[0] = region->extent_xhi;
-    domain->boxlo[1] = region->extent_ylo;
-    domain->boxhi[1] = region->extent_yhi;
-    domain->boxlo[2] = region->extent_zlo;
-    domain->boxhi[2] = region->extent_zhi;
-  }
+  domain->triclinic = 0;
+  domain->boxlo[0] = region->extent_xlo;
+  domain->boxhi[0] = region->extent_xhi;
+  domain->boxlo[1] = region->extent_ylo;
+  domain->boxhi[1] = region->extent_yhi;
+  domain->boxlo[2] = region->extent_zlo;
+  domain->boxhi[2] = region->extent_zhi;
   atom->ntypes = utils::inumeric(FLERR, arg[0], false, lmp);
   int iarg = 2;
   update->ntimestep = 0;
